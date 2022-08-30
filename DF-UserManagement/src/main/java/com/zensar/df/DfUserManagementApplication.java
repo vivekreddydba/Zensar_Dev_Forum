bb7f03bb4957f9749aa34118466b0967ae341977
YD69425 <YD69425@ADMW46ZLPC1628.ind.zensar.com>
2022-08-30 15:23:20 +0530

    User Management Project Setup

---
+++ DF-UserManagement/src/main/java/com/zensar/df/DfUserManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfUserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfUserManagementApplication.class, args);
+	}
+
+}
