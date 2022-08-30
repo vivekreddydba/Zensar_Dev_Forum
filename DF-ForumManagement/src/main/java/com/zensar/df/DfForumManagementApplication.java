ee77c02fde895e0d7c993739e0c888e55c044ace
YD69425 <YD69425@ADMW46ZLPC1628.ind.zensar.com>
2022-08-30 15:38:22 +0530

    Forum Management Project Setup

---
+++ DF-ForumManagement/src/main/java/com/zensar/df/DfForumManagementApplication.java
@@ -0,0 +1,13 @@
+package com.zensar.df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfForumManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfForumManagementApplication.class, args);
+	}
+
+}
---
+++ DF-ForumManagement/src/test/java/com/zensar/df/DfForumManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.df;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfForumManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
