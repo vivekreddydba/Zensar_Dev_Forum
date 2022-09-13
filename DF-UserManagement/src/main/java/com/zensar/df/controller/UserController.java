4cd5a443c89453e9ed08a841cd56dd71f0cd09bc
BV69489 <BV69489@ADMW46ZLPC1640.ind.zensar.com>
2022-09-13 18:56:10 +0530

    return user information

--- DF-UserManagement/src/main/java/com/zensar/df/controller/UserController.java
+++ DF-UserManagement/src/main/java/com/zensar/df/controller/UserController.java
@@ -143,7 +143,7 @@ public class UserController {
 			catch(Exception e) {
 				return new ResponseEntity<UserDto>(HttpStatus.BAD_REQUEST);
 			}
-			if(this.isTokenValid(jwtToken)== new ResponseEntity<Boolean>(true,HttpStatus.OK)) {
+			if(isTokenValid) {
 				user = userService.findUserByUsername(username).get(0);		
 				return new ResponseEntity<UserDto>(user,HttpStatus.ACCEPTED);
 			}
